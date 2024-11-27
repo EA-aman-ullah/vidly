282f3e57fdb10ca09f35327858b748978c713239
EA-aman-ullah <hiamanullahkodex@gmail.com>
2024-11-27 13:59:22 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const express = require("express");
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDb..."))
+  .catch((err) => console.log("Could not connect to MongoDb..." + err));
+
+app.use(express.json());
+app.use("/api/genres", genres);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
